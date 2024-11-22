239b8624ef903065c064d181c8359ab9fe9d232e
Matheus Henrique de Andrade Pellegrini <139627645+MatheusAndrade03@users.noreply.github.com>
2024-11-22 10:24:39 -0300

    att

--- home/home.js
+++ home/home.js
@@ -43,7 +43,7 @@ const btnSair = document.querySelector(".btn-sair");
 
 
 btnSair.addEventListener("click", function(){
-    window.location.href = "../login/login.html";
+    window.location.href = "../index.html";
 });
 
 
